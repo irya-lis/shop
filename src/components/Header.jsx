28dccde9ecdd01bde803f7901fc3919c67a7ab37
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-11-10 17:34:04 +0300

    add gh-pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="nav-wrapper">
                 <a href="/" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://github.com/irya-lis/shop" target="_blank" rel="noreferrer">Repo</a></li>
+                    <li><a href="https://github.com/irya-lis/shop" target="_blank">Repo</a></li>
                 </ul>
             </div>
         </nav>
